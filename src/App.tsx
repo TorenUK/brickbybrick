40ac9e0ddb22f6554f861635754ac4a36dbb71a5
TorenUK <toren@toren.uk>
2024-05-06 16:08:22 +0100

    chore: base files

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="font-bold text-2xl w-screen h-screen flex justify-center items-center">
+        hello from brick by brick
+      </div>
+    </>
+  );
+}
+
+export default App;
