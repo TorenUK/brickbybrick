cd1e7c308cc41ecef78ff67a3b73551f818f7967
TorenUK <toren@toren.uk>
2024-05-08 15:18:53 +0100

    chore: add firebase config

---
+++ firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: `${import.meta.env.VITE_FIREBASE_API_KEY}`,
+  authDomain: "brickbybrick-2d8e4.firebaseapp.com",
+  projectId: "brickbybrick-2d8e4",
+  storageBucket: "brickbybrick-2d8e4.appspot.com",
+  messagingSenderId: `${import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID}`,
+  appId: "1:772808793114:web:9a025016ffdce0016405a3",
+  measurementId: "G-HLW985LYH3",
+};
+
+export default firebaseConfig;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import firebaseConfig from "../firebase.config.js";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
