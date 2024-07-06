51b212e6cbb32e4cdc4f88656a3ff0f12caf624b
TorenUK <toren@toren.uk>
2024-07-06 11:38:20 +0100

    chore: init deploy

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <>
       <div className="font-bold text-2xl w-screen h-screen flex justify-center items-center">
-        hello from brick by brick
+        brickbybrick.dev
       </div>
     </>
   );
--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,13 @@ import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import firebaseConfig from "../firebase.config.js";
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
+initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
