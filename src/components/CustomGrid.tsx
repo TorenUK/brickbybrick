c517f8ec5b55ca384a136bce03260200550053f5
TorenUK <toren@toren.uk>
2024-12-08 20:15:50 +0000

    fix: temp responsiveness

--- src/components/CustomGrid.tsx
+++ src/components/CustomGrid.tsx
@@ -91,71 +91,71 @@ export const renderGrid = () => {
 
 const CustomGrid = () => {
   return (
-    <div className="relative h-screen w-screen grid grid-cols-5 grid-rows-7 gap-2 pt-4 pl-4 pr-4">
+    <div className="relative h-screen w-screen grid grid-cols-2 grid-rows-5 md:grid-cols-5 md:grid-rows-7 gap-2 pt-4 pl-4 pr-4">
       <div className="col-span-3 sm:col-span-2">
         <Hero />
       </div>
-      <div className="card-wrapper">
+      {/* <div className="card-wrapper hidden md:block ">
         <div className="card-content">4</div>
-      </div>
-      <div className="card-wrapper col-start-4">
+      </div> */}
+      <div className="card-wrapper col-start-4 hidden md:block">
         <div className="card-content">5</div>
       </div>
-      <div className="card-wrapper col-start-5">
+      <div className="card-wrapper hidden md:block">
         <div className="card-content">6</div>
       </div>
-      <div className="card-wrapper col-span-2 row-start-2">
-        <div className="card-content flex items-center justify-center">
+      <div className="card-wrapper col-span-3 md:col-span-2 row-start-2">
+        <div className="card-content flex items-center justify-center text-md">
           who we are.
         </div>
       </div>
-      <div className="card-wrapper col-span-2 col-start-4 row-start-2">
-        <div className="card-content flex items-center justify-center">
+      <div className="card-wrapper hidden md:flex col-span-2 col-start-4 row-start-2">
+        <div className="card-content flex items-center justify-center text-md">
           what we do.
         </div>
       </div>
-      <div className="card-wrapper col-start-3 row-start-2">
+      <div className="card-wrapper hidden md:block col-start-3 row-start-2">
         <div className="card-content">12</div>
       </div>
-      <div className="card-wrapper col-span-3 row-span-3 col-start-2 row-start-3">
-        <div className="card-content flex items-center justify-center">
+      <div className="card-wrapper col-start-1 col-span-3 row-span-2 md:col-span-3 md:row-span-3 md:col-start-2 row-start-3">
+        <div className="card-content flex items-center justify-center text-md">
           animated content.
         </div>
       </div>
-      <div className="card-wrapper col-start-1 row-start-3">
+      <div className="card-wrapper hidden md:block col-start-1 row-start-3">
         <div className="card-content">14</div>
       </div>
-      <div className="card-wrapper col-start-1 row-start-4">
+      <div className="card-wrapper hidden md:block col-start-1 row-start-4">
         <div className="card-content">15</div>
       </div>
-      <div className="card-wrapper col-start-1 row-start-5">
+      <div className="card-wrapper hidden md:block col-start-1 row-start-5">
         <div className="card-content">16</div>
       </div>
-      <div className="card-wrapper col-start-5 row-start-3">
+      <div className="card-wrapper hidden md:block col-start-5 row-start-3">
         <div className="card-content">17</div>
       </div>
-      <div className="card-wrapper col-start-5 row-start-4">
+      <div className="card-wrapper hidden md:block col-start-5 row-start-4">
         <div className="card-content">18</div>
       </div>
-      <div className="card-wrapper col-start-5 row-start-5">
+      <div className="card-wrapper hidden md:block col-start-5 row-start-5">
         <div className="card-content">19</div>
       </div>
-      <div className="card-wrapper col-start-1 row-start-6 col-span-2 ">
-        <div className="card-content flex items-center justify-center">
+      <div className="card-wrapper hidden md:block col-start-1 row-start-6 col-span-2 ">
+        <div className="card-content flex items-center justify-center text-md">
           technologies.
         </div>
       </div>
-      <div className="card-wrapper col-start-3 row-start-6 col-span-1 ">
-        <div className="card-content flex items-center justify-center">
+      <div className="card-wrapper hidden md:block col-start-3 row-start-6 col-span-1 ">
+        <div className="card-content flex items-center justify-center text-md">
           technologies.
         </div>
       </div>
-      <div className="card-wrapper col-start-4 row-start-6 col-span-2 ">
-        <div className="card-content flex items-center justify-center">
+      <div className="card-wrapper hidden md:block col-start-4 row-start-6 col-span-2 ">
+        <div className="card-content flex items-center justify-center text-md">
           technologies.
         </div>
       </div>
-      <div className="card-wrapper col-start-1 row-start-7 col-span-5 ">
+      <div className="card-wrapper col-start-1 col-span-3 md:row-start-7 md:col-span-5 ">
         <div className="card-content flex flex-col justify-end">
           <Footer />
         </div>
--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-black text-white text-center p-2">
+    <footer className="bg-black text-white text-center p-2 text-sm">
       <p>2024 brickbybrick</p>
     </footer>
   );
