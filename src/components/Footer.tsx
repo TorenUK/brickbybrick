3fbb4eb2af3047ed0e27c3c89e9f07af59b81305
TorenUK <toren@toren.uk>
2024-12-08 20:45:08 +0000

    fix: footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return (
-    <footer className="bg-black text-white text-center p-2 text-sm">
+    <footer className="col-span-3 sm:col-span-5 bg-black text-white text-center text-sm p-2">
       <p>2024 brickbybrick</p>
     </footer>
   );
