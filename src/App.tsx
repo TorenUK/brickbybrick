c98fa1835c3e9a2406263c5563eae19cf8ec3dba
TorenUK <toren@toren.uk>
2025-01-19 19:53:01 +0000

    feat: cleanup underlay

--- src/App.tsx
+++ src/App.tsx
@@ -2,24 +2,29 @@ import { GridLayoutMobile, GridlayoutDesktop } from "./components/GridLayout";
 // import { TestDesktop, TestMobile } from "./components/TestDynamicGrid";
 
 function App() {
+  const renderBackgroundGrid = () => {
+    const gridCount = 7;
+    return Array.from({ length: gridCount }, (_, index) => (
+      <div
+        key={index}
+        className="border-x-[1px] border-dotted h-full md:w-1/6 w-1/4 opacity-5"
+      ></div>
+    ));
+  };
+
   return (
     <div className="app">
-      <div className="absolute w-screen h-screen -z-10 flex flex-row ">
-        <div className="border-[1px] border-dotted h-full md:w-1/6 w-1/4 opacity-10"></div>
-        <div className="border-[1px] border-dotted h-full md:w-1/6 w-1/4 opacity-10"></div>
-        <div className="border-[1px] border-dotted h-full md:w-1/6 w-1/4 opacity-10"></div>
-        <div className="border-[1px] border-dotted h-full md:w-1/6 w-1/4 opacity-10"></div>
-        <div className="border-[1px] border-dotted h-full md:w-1/6 w-1/4 opacity-10"></div>
-        <div className="border-[1px] border-dotted h-full md:w-1/6 w-1/4 opacity-10"></div>
-        {/* <div className="border-[1px] border-dotted h-full md:w-1/6 w-1/4 opacity-10"></div> */}
+      {/* Background grid */}
+      <div className="absolute w-screen h-screen -z-10 flex flex-row">
+        {renderBackgroundGrid()}
       </div>
+
+      {/* Layout rendering based on screen size */}
       <div className="hidden md:block">
         <GridlayoutDesktop />
-        {/* <TestDesktop /> */}
       </div>
       <div className="block md:hidden">
         <GridLayoutMobile />
-        {/* <TestMobile /> */}
       </div>
     </div>
   );
