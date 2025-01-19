866ec69be0492513cdbc3b1645dfab5a77152bf0
TorenUK <toren@toren.uk>
2025-01-19 19:28:32 +0000

    feat: test underlay

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,16 @@ import { GridLayoutMobile, GridlayoutDesktop } from "./components/GridLayout";
 
 function App() {
   return (
-    <>
+    <div className="app">
+      <div className="absolute w-screen h-screen -z-10 flex flex-row ">
+        <div className="border-[1px] border-dotted h-full md:w-1/6 w-1/4 opacity-10"></div>
+        <div className="border-[1px] border-dotted h-full md:w-1/6 w-1/4 opacity-10"></div>
+        <div className="border-[1px] border-dotted h-full md:w-1/6 w-1/4 opacity-10"></div>
+        <div className="border-[1px] border-dotted h-full md:w-1/6 w-1/4 opacity-10"></div>
+        <div className="border-[1px] border-dotted h-full md:w-1/6 w-1/4 opacity-10"></div>
+        <div className="border-[1px] border-dotted h-full md:w-1/6 w-1/4 opacity-10"></div>
+        {/* <div className="border-[1px] border-dotted h-full md:w-1/6 w-1/4 opacity-10"></div> */}
+      </div>
       <div className="hidden md:block">
         <GridlayoutDesktop />
         {/* <TestDesktop /> */}
@@ -12,7 +21,7 @@ function App() {
         <GridLayoutMobile />
         {/* <TestMobile /> */}
       </div>
-    </>
+    </div>
   );
 }
 
